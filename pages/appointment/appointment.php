92ac5534b0002846e2f52cb9442c26bb864f05ad
cywpat <cywpatricia.2000@gmail.com>
2022-10-20 17:23:20 +0800

    pages

--- pages/appointment/appointment.php
+++ pages/appointment/appointment.php
@@ -7,11 +7,58 @@
     <title>Best Smile Dental Clinic - Appointment</title>
     <link rel="stylesheet" href="../../styles/global.css">
     <link rel="shortcut icon" href="../../images/favicon.ico" type="image/x-icon">
+    <style type="text/css">
+      * {
+        box-sizing: border-box;
+      }
+      .col-left {
+        flex: 30%;
+        border: 2px solid;
+        padding: 25px;
+        height: 300px;
+      }
+      .col-left img {
+        float: left;
+        border: 2px solid;
+      }
+      .col-left-top {
+        float: left;
+        border: 2px solid;
+      }
+      .col-left-intro {
+        float: left;
+        border: 2px solid;
+      }
+      .col-right {
+        flex: 70%;
+        border: 2px solid;
+        padding: 25px;
+      }
+      /* Responsive layout - when the screen is less than 700px wide, make the two columns stack on top of each other instead of next to each other */
+      @media screen and (max-width: 700px) {
+        .content {   
+          flex-direction: column;
+        }
+      }
+    </style>
   </head>
   <body>
     <?php include '../../components/header.php';?>
     <div class="content">
-
+      <div class="col-left">
+        <img src="#" alt="Dentist">
+        <div class="col-left-top">
+          <h3>Dentist Name</h3>
+          <p>Position</p>
+        </div>
+        <div class="col-left-intro">
+          <p>Specialisation</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper ultricies nunc, vel sollicitudin odio finibus in. Praesent quis velit dolor.</p>
+        </div>
+      </div>
+      <div class="col-right">
+        <h3 style="text-align: center;">Select a Date & Time</h3>
+      </div>
     </div>
     <?php include '../../components/footer.php';?>
   </body>
--- pages/home/home.php
+++ pages/home/home.php
@@ -7,10 +7,56 @@
     <title>Best Smile Dental Clinic - Home</title>
     <link rel="stylesheet" href="../../styles/global.css">
     <link rel="shortcut icon" href="../../images/favicon.ico" type="image/x-icon">
+    <style type="text/css">
+      * {
+        box-sizing: border-box;
+      }
+      .col-left {
+        flex: 50%;
+        border: 2px solid;
+        padding: 25px;
+      }
+      .col-left img {
+        background-color: #aaa;
+        width: 100%;
+        padding: 20px;
+      }
+      .col-right {
+        flex: 50%;
+        border: 2px solid;
+        padding: 25px;
+      }
+      .col-right button {
+        background-color: lightblue;
+        width: 200px;
+        height: 40px;
+        font-size: 16px;
+        font-weight: bold;
+        border: 0px;
+        margin-right: 25px;
+      }
+      /* Responsive layout - when the screen is less than 700px wide, make the two columns stack on top of each other instead of next to each other */
+      @media screen and (max-width: 700px) {
+        .content {   
+          flex-direction: column;
+        }
+      }
+    </style>
   </head>
   <body>
     <?php include '../../components/header.php';?>
     <div class="content">
+      <div class="col-left">
+        <img src="" alt="Home Image" style="height: 200px;">
+      </div>
+      <div class="col-right">
+        <p>Welcome to</p>
+        <h1>Best Smile Dental Clinic Online Booking System</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec lectus sit amet sapien viverra congue. Praesent consectetur tortor et efficitur venenatis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec lectus sit amet sapien viverra congue.</p>
+        <p>Call us +65 12345678 to find out more.</p>
+        <a href="../login page/login.php"><button type="button" name="login" id="login">Login</button></a>
+        <a href="../register page/register.php"><button type="button" name="signup" id="signup">Sign Up</button></a>
+      </div>
 
     </div>
     <?php include '../../components/footer.php';?>

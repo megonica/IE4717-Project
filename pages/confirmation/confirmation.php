12b1be4be60fae228223fdb4b8448f9a0b83003c
cywpat <cywpatricia.2000@gmail.com>
2022-10-31 11:12:23 +0800

    confirmation html css

--- pages/confirmation/confirmation.php
+++ pages/confirmation/confirmation.php
@@ -7,11 +7,26 @@
     <title>Best Smile Dental Clinic - Confirmation</title>
     <link rel="stylesheet" href="../../styles/global.css">
     <link rel="shortcut icon" href="../../images/favicon.ico" type="image/x-icon">
+    <style>
+      .content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+    </style>
   </head>
   <body>
     <?php include '../../components/header.php';?>
     <div class="content">
-
+      <h1>Appointment Confirmation</h1>
+      <p>ABC, your appointment has been confirmed. The details are as follows:</p>
+      <h3>Dr. Smile</h3>
+      <h3>Wednesday, 24 April 2022</h3>
+      <h3>2:00pm</h3>
+      <p>A confirmation email has been sent to:</p>
+      <h3>customeremail@email.com</h3>
+      <p>Check your email to ensure that you have received the appointment confirmation email.</p>
     </div>
     <?php include '../../components/footer.php';?>
   </body>

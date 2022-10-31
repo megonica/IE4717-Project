56a3281693b96df0b1938b83bf78351b113d446d
cywpat <cywpatricia.2000@gmail.com>
2022-10-31 11:11:46 +0800

    footer

--- components/footer.php
+++ components/footer.php
@@ -1,4 +1,4 @@
 <footer class="footer">
-    <p>Copyright (C) 2022 All Rights Reserved | Best Smile Dental Clinic Online Booking System
+    <p>Copyright &copy 2022 All Rights Reserved | Best Smile Dental Clinic Online Booking System
     <br/>Done by Megan & Patricia</p>
 </footer>
